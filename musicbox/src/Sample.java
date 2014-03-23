1ba222f42418fdf59d7a5a5d47b6adb2c2560510
taloto07 <taloto07@gmail.com>
2014-03-23 00:27:00 -0700

    delete sample.java and change test.java

--- musicbox/src/Sample.java
+++
@@ -1,8 +0,0 @@
-
-public class Sample {
-
-	public static void main(String[] args){
-		System.out.println("Dont worry about man. you'll get it.");
-	}
-	
-}
--- musicbox/src/test.java
+++ musicbox/src/test.java
@@ -4,11 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
-		System.out.println("hello world2!");
-		System.out.println("hello world3!");
-		System.out.println("hello world4!");
 		
-		System.out.println("hello world15!");
 	}
 
 }
